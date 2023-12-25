b8eb056903cb2d3ff27173eb9a7d1888396e68fd
Michel San Martin <sanmartin.michels@gmail.com>
2023-12-25 20:43:40 -0300

    separando responsabilidades, index.js como orquestador de toda la aplicación de backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+const {app} = require('./app.js');
+const {run} = require('./db.js')
+
+app.listen(3000);
+console.log('SERVER ON PORT ', 3000);
+
+run()
+
 console.log("probando probando");
+
+console.log("segunda prueba");
